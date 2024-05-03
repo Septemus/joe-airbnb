4af037ef0c3afc95f7023797116bc36abda1d4f2
jingjiezhou <jingjiezhou@tencent.com>
2024-05-03 16:49:06 +0800

    general:add commitlint

---
+++ commitlint.config.ts
@@ -0,0 +1,26 @@
+import {UserConfig} from '@commitlint/types'; 
+import {RuleConfigSeverity} from '@commitlint/types'; 
+const ret:UserConfig= {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'msg-rule': [RuleConfigSeverity.Error, 'always'],
+        'subject-empty':[RuleConfigSeverity.Disabled, 'never'],
+        'type-empty':[RuleConfigSeverity.Disabled, 'never'],
+    },
+    
+    plugins: [
+        {
+            rules: {
+                'msg-rule': (subject) => {
+                    console.log(subject)
+                    const ret=/^(general|web|server):.*/.test(subject.header)
+                    return [
+                        ret,
+                        `Your subject should contain start with 'general|web|server'`,
+                    ];
+                },
+            },
+        },
+    ],
+};
+export default ret
