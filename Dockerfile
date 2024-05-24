f8b3e7a5e8c86c8ab0af32a53221bcc79c6f1f66
joe <musketeerdt@gmail.com>
2024-05-24 19:11:04 +0800

    general:adjust to dokku environment

--- Dockerfile
+++ Dockerfile
@@ -5,14 +5,12 @@ WORKDIR /abb
 COPY ./package.json .
 COPY ./packages/server/package.json ./packages/server/
 COPY ./packages/common/package.json ./packages/common/
-COPY ./packages/controllers/package.json ./packages/controllers/
 
 
 RUN yarn --production
 
 COPY ./packages/server/dist/ ./packages/server/dist/
 COPY ./packages/common/dist/ ./packages/common/dist/
-COPY ./packages/controllers/dist/ ./packages/controllers/dist/
 
 COPY ./packages/server/.env.prod ./packages/server/
 COPY ./ormconfig.json .
--- packages/server/src/utils/createTypeormConn.ts
+++ packages/server/src/utils/createTypeormConn.ts
@@ -1,6 +1,16 @@
-import { getConnectionOptions, createConnection } from 'typeorm';
+import {
+  getConnectionOptions,
+  createConnection,
+  ConnectionOptions
+} from 'typeorm';
 
 export const createTypeormConn = async () => {
   const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
-  return createConnection({ ...connectionOptions, name: 'default' });
+  return process.env.NODE_ENV === 'production'
+    ? createConnection({
+        ...connectionOptions,
+        url: process.env.DATABASE_URL,
+        name: 'default'
+      } as ConnectionOptions)
+    : createConnection({ ...connectionOptions, name: 'default' });
 };
